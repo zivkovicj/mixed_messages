6f096b61748df0303dba40214efb95debcdee176
zivkovicj <zivkovic.jeff@gmail.com>
2022-05-15 09:50:44 -0600

    It's working now

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 const quotes = [
     ["Before you marry a person you should first make them use a computer with slow Internet service to see who they really are.","person","computer","marry"],
-    ["If you find a good wife you’ll be happy If not you’ll become a philosopher.","wife","philosopher","become"],
-    ["Adults are always asking children what they want to be when they grow up because they’re looking for ideas.","children","ideas","looking"]
+    ["If you find a good wife you’ll be happy If not you’ll become a philosopher","wife","philosopher","become"],
+    ["Adults are always asking children what they want to be when they grow up because they’re looking for ideas","children","ideas","looking"]
 ];
 
-const randomNum = Math.floor(Math.random() * quotes.length);
+const nounElem1 = document.getElementById('noun-one');
+const nounElem2 = document.getElementById('noun-two');
+const verbElem = document.getElementById('verb');
+const button = document.getElementById('button');
+const sillyQuote = document.getElementById('silly-quote');
 
-let testSentence = quotes[randomNum][0].split(' ');
-const keyWord1 = quotes[randomNum][1];
-const keyWord2 = quotes[randomNum][2];
-const keyWord3 = quotes[randomNum][3];
+button.onclick = function() {
+    const randomNum = Math.floor(Math.random() * quotes.length);
 
-const nounOne = document.getElementById(noun-one);
-const nounTwo = document.getElementById(noun-two);
-const verb = document.getElementById(verb);
-const button = document.getElementById(button);
+    let origSentence = quotes[randomNum][0].split(' ');
+    const keyWord1 = quotes[randomNum][1];
+    const keyWord2 = quotes[randomNum][2];
+    const keyWord3 = quotes[randomNum][3];
 
+    sillyQuote.hidden = false;
 
-testSentence = testSentence.map(word => {
-    if (word === keyWord1) {
-        return nounOne;
-    } else if (word === keyWord2) {
-        return nounTwo;
-    } else if (word === keyWord3) {
-        return verb;
-    } else {
-        return word;
-    }
-})
+    nounOne = nounElem1.value;
+    nounTwo = nounElem2.value;
+    verb = verbElem.value;
 
-console.log(testSentence);
+    origSentence = origSentence.map(word => {
+        if (word === keyWord1) {
+            return nounOne;
+        } else if (word === keyWord2) {
+            return nounTwo;
+        } else if (word === keyWord3) {
+            return verb;
+        } else {
+            return word;
+        }
+    })
+
+    sillyQuote.innerHTML = origSentence.join(' ');
+}
+
 
